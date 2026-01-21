cf7bfe800c14638ef4f9af966ee46445a86a03ee
ismisebrendan <brendanwatters10@gmail.com>
2026-01-21 11:57:50 +0000

    Added time_print.* files to edit and the edited time_save.* files which save the sequence of numbers and time taken to a data file and also output a total time

---
+++ week2/time_save.py
@@ -0,0 +1,41 @@
+import sys
+import time
+
+def main(): 
+    output = 0
+    # checks if there are the right number of arguments
+    try:
+        # converts the first two arguments to integers
+        in_arg = int(sys.argv[1])
+    except: # (argc != 2)
+        # raises an error
+        raise Exception("Incorrect arguments.\nUsage: python time_print.py [NUM]\ne.g.\npython time_print.py 23")
+
+    # gets the start time for the loop
+    start_time = time.time()
+    
+    # open or create a new file
+    f = open("/home/ug/wattersb2/data/week2/python_time_out.txt", "w")
+
+    # iterates over all numbers up to the input
+    for i in range(in_arg):
+        # writes the index to the file
+        f.write(f"{i}, ")
+    # gets the end time for the loop
+    end_time = time.time()
+
+    # gets the total time
+    run_time = end_time - start_time
+    # writes the output to the file
+    f.write("\n\nTime for loop: {} seconds\n".format(run_time))
+
+    # close the file
+    f.close()
+
+    # get the total time, including saving the file
+    run_time = end_time - start_time
+    # print the output
+    print("\n\nTime for loop: {} seconds\n".format(run_time))
+
+if __name__ == "__main__":
+    main()
