ab0b65c1ff876c35ee60598999abff02b383f08a
ismisebrendan <brendanwatters10@gmail.com>
2026-02-01 12:05:23 +0000

    Added hello world programs

---
+++ week3/hello_world_mpi_messing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+	int myRank, uniSize, ierror;
+
+	// set up MPI
+
+	// get rank and world size
+	ierror = MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	ierror = MPI_Comm_size(MPI_COMM_WORLD, &uniSize);
+
+	ierror = MPI_Init(&argc, &argv);
+	// print to screen
+	printf("Hello world, I am process %d of %d\n", myRank, uniSize);
+
+	// close MPI connections
+	ierror = MPI_Finalize();
+	return 0;
+}
