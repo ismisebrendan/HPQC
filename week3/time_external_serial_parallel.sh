cff2e4a258389408f4861eaedc5dbdfa01906e3c
ismisebrendan <brendanwatters10@gmail.com>
2026-02-05 14:24:16 +0000

    Updated external timing shell script to give a simplified output

--- week3/time_external_serial_parallel.sh
+++ week3/time_external_serial_parallel.sh
@@ -7,8 +7,8 @@
 # Serial case
 for i in {10..1861..10}; do
 	echo "$i" >> data/serial_external_timing.txt
-	# Call the time function, output to a file and append to it. Save the data with the format: Real, usr, sys. Call the c funtion with $a as its input
-	\time -o data/serial_external_timing.txt --append -f "%e, %U, %S" ./bin/vector_serial_edited $i
+	# Call the time function, output to a file and append to it. Save the real time. Call the c funtion with $a as its input
+	\time -o data/serial_external_timing.txt --append -f "%e" ./bin/vector_serial_edited $i
 done
 
 # Parallel case
@@ -16,6 +16,6 @@ for j in {2..16}; do
 	echo "$j" >> data/parallel_external_timing.txt
 	for i in {10..1861..10}; do
 		echo "$i" >> data/parallel_external_timing.txt
-		\time -o data/parallel_external_timing.txt --append -f "%e, %U, %S" mpirun -np $j ./bin/vector_parallel $i
+		\time -o data/parallel_external_timing.txt --append -f "%e" mpirun -np $j ./bin/vector_parallel $i
 	done
 done
