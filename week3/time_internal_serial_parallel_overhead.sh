89631d36f5ae3eddda6f1a3a7c3bc4764c596f68
ismisebrendan <brendanwatters10@gmail.com>
2026-02-05 14:45:48 +0000

    Added files to time, including the overhead

---
+++ week3/time_internal_serial_parallel_overhead.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run the file repeatedly with different numbers input
+# Using internal timing for benchmarking
+
+# Test for values from 10 to 1860
+# Serial case
+for i in {10..1861..10}; do
+	./bin/vector_serial_internal_overhead $i
+done
+
+# Parallel case
+for j in {2..16}; do
+	for i in {10..1861..10}; do
+		mpirun -np $j ./bin/vector_parallel_internal_overhead $i
+	done
+done
