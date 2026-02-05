dc21dac120ff815c6c6e675b0dcb184c44f9ecb9
Brendan <wattersb@tcd.ie>
2026-02-05 15:48:59 +0000

    Update plotting_serial_parallel.py

--- week3/plotting_serial_parallel.py
+++ week3/plotting_serial_parallel.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 
 # Plot the time for each method separately
 
-# Internal
+# No overhead
 internal_serial = np.genfromtxt('./data/serial_timing_internal.txt', delimiter=',')
 internal_parallel = np.genfromtxt('./data/parallel_timing_internal.txt', delimiter=',')
-
-
 
 # Plot each number of processes separately
 for i in range(2, 17):
@@ -34,3 +32,48 @@ for i in range(2, 17):
     # plt.show()
     plt.close()
 
+# Overhead
+overhead_serial = np.genfromtxt('./data/serial_timing_internal_overhead.txt', delimiter=',')
+overhead_parallel = np.genfromtxt('./data/parallel_timing_internal_overhead.txt', delimiter=',')
+
+# Plot the serial process on its own
+plt.plot(overhead_serial[:,0], overhead_serial[:,1], marker='o', markersize=3, label='Serial Internal')
+
+plt.grid()
+plt.legend()
+
+plt.title('Time recorded internally for serial, including overhead')
+plt.tight_layout()
+plt.savefig(f'./data/internal_plot_serial_overhead.png')
+# plt.show()
+
+# Plot with logarithmic axes
+plt.yscale('log')
+plt.xscale('log')
+plt.tight_layout()
+plt.savefig(f'./data/internal_plot_serial_overhead_log.png')
+# plt.show()
+plt.close()
+
+# Plot each number of processes separately
+for i in range(2, 17):
+    plt.plot(overhead_serial[:,0], overhead_serial[:,1], marker='o', markersize=3, label='Serial Internal')
+
+    parallel_plot = overhead_parallel[overhead_parallel[:,0]==i]
+    plt.plot(parallel_plot[:,1], parallel_plot[:,2], marker='o', markersize=3, label=f'Parallel Internal np={i}')
+
+    plt.grid()
+    plt.legend()
+
+    plt.title('Time recorded internally, including overhead')
+    plt.tight_layout()
+    plt.savefig(f'./data/internal_plot_{i}_processors_overhead.png')
+    # plt.show()
+    
+    # Plot with logarithmic axes
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.tight_layout()
+    plt.savefig(f'./data/internal_plot_{i}_processors_overhead_log.png')
+    # plt.show()
+    plt.close()
