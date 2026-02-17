79ac685bd89adbc5df0ed90c857c82696fa878b3
ismisebrendan <brendanwatters10@gmail.com>
2026-02-17 12:00:24 +0000

    Updated run_pingpong_vector.sh to not try to start from 0

--- week4/run_pingpong_vector.sh
+++ week4/run_pingpong_vector.sh
@@ -4,7 +4,7 @@
 # Using internal timing for benchmarking
 
 # Test for values from 1 to 524288 (equivalent to a vector 2 MiB long)
-for i in {0..524289..256}; do
+for i in {4..524289..256}; do
 
 	mpirun -np 2 ./bin/pingpong_vector $i
 done
