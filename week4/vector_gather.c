438773e6fa3f8af2a71ba0c3e2072e65b90d8f66
ismisebrendan <brendanwatters10@gmail.com>
2026-02-19 17:47:45 +0000

    Added file that uses MPI_Gather to get the partial sums of the vector components

---
+++ week4/vector_gather.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+// Declare the functions that will be called within main
+int check_args(int argc, char **argv);
+void initialise_vector(int vector[], int size, int initial);
+void print_vector(int vector[], int size);
+int sum_vector_p(int vector[], int size, int my_rank, int uni_size);
+void check_uni_size(int uni_size);
+
+int main(int argc, char **argv)
+{
+	// Error handling variable
+	int ierror = 0;
+
+	// Declare and initialise the numerical argument variable
+	int num_arg = check_args(argc, argv);
+
+	// Intialise MPI
+	ierror = MPI_Init(&argc, &argv);
+
+	// Declare and initialise rank and size
+	int my_rank, uni_size;
+	my_rank = uni_size = 0;
+
+	// Get the rank and world size
+	ierror = MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	ierror = MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
+	
+	// Check the universe size is correct
+	check_uni_size(uni_size);
+	
+	// Create a vector variable
+	int* my_vector = malloc (num_arg * sizeof(int));
+	// And initialise every element
+	initialise_vector(my_vector, num_arg, 0);
+
+	// Find the sum in parallel
+	sum_vector_p(my_vector, num_arg, my_rank, uni_size);
+
+	// Finalise MPI
+	ierror = MPI_Finalize();
+
+	// If we use malloc, must free when done!
+	free(my_vector);
+
+	return 0;
+}
+
+// Sum the vector in parallel
+int sum_vector_p(int vector[], int size, int my_rank, int uni_size)
+{
+	// Store the partial sum
+	int partial_sum = 0;
+
+	// Initialise a vector to take the sums from each process
+	int* local_sums = malloc(uni_size * sizeof(int));
+
+	// Initialise stop
+	int stop;
+
+	// Break up the vector
+	int chunk = size / uni_size;
+
+	int start = my_rank * chunk;
+
+	// If this is the last process take all elements at the end as well
+	if (uni_size - 1 == my_rank)
+	{
+		stop = size;
+	}
+	else // otherwise take up to the start of the next chunk
+	{
+		stop = (my_rank + 1) * chunk;
+	}
+
+	// Sum this specific chunk
+	for (int i = start; i < stop; i++)
+	{
+		partial_sum += vector[i];
+	}
+
+	// Transmission variables
+	int count, root;
+	count = 1;
+	root = 0;
+
+	MPI_Gather(&partial_sum, count, MPI_INT, local_sums, count, MPI_INT, root, MPI_COMM_WORLD);
+
+	// Now sum them all
+	if (my_rank == 0)
+	{
+		// The final sum
+		int total_sum = 0;
+
+		// For all  ranks
+		for (int i = 0; i < uni_size; i++)
+		{
+			// Add to the running total
+			total_sum += local_sums[i];
+		}
+
+		// Output and return
+		printf("Sum: %d\n", total_sum);
+		return total_sum;
+	}
+	free(local_sums);
+}
+
+// Define a function to initialise all values in a vector
+void initialise_vector(int vector[], int size, int initial)
+{
+	// Iterates through the vector
+	for (int i = 0; i < size; i++)
+	{
+		// Set the elements of the vector to the (initial value + i)**2
+		vector[i] = (initial + i)*(initial + i);
+	}
+}
+
+// Define a function to print a vector of ints
+void print_vector(int vector[], int size)
+{
+	// Iterate through the vector
+	for (int i = 0; i < size; i++)
+	{
+		// Print the elements of the vector to the screen
+		printf("%d\n", vector[i]);
+	}
+}
+
+// Define a function that checks your arguments to make sure they'll do what you need
+int check_args(int argc, char **argv)
+{
+	// Declare and initialise the numerical argument
+	int num_arg = 0;
+
+	// Check the number of arguments
+	if (argc == 2) // program name and numerical argument
+	{
+		// Declare and initialise the numerical argument
+		num_arg = atoi(argv[1]);
+	}
+	else // The number of arguments is incorrect
+	{
+		// Raise an error
+		fprintf(stderr, "ERROR: You did not provide a numerical argument!\n");
+		fprintf(stderr, "Correct use: %s [NUMBER]\n", argv[0]);
+
+		// And exit COMPLETELY
+		exit (-1);
+	}
+	return num_arg;
+}
+
+// Ensure the universe size is ok
+void check_uni_size(int uni_size)
+{
+	// Set the minimum universe size
+	int min_uni_size = 1;
+	// Check there are sufficient tasks to communicate with
+	if (uni_size >= min_uni_size)
+	{
+		return;
+	} 
+	else
+	{
+		// Raise an error
+		fprintf(stderr, "Unable to communicate with fewer than %d processes.", min_uni_size);
+		fprintf(stderr, "MPI communicator size = %d\n", uni_size);
+
+		// And exit COMPLETELY
+		exit(-1);
+	}
+}
