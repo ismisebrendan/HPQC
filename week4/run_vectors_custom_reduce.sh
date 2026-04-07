4b41fd6d923c8c53e34435dffacadc45838bff24
ismisebrendan <brendanwatters10@gmail.com>
2026-04-07 12:29:21 +0100

    Added file to repeatedly run vector_custom and vector_reduce for various inputs

---
+++ week4/run_vectors_custom_reduce.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run the file repeatedly with different numbers input
+# Using internal timing for benchmarking
+
+# Test for 2 to 16 processors
+for j in {2..16}; do
+	# Test for number of elements from 10 to 510
+	for i in {10..510..20}; do
+		# Repeat 10 times to get a mean
+		for k in {1..10}; do
+			mpirun -np $j ./bin/vector_reduce $i
+		done
+	done
+done
