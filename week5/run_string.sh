7a532607449d8228abdf1d89d960f089a52acb51
ismisebrendan <brendanwatters10@gmail.com>
2026-04-13 22:09:03 +0100

    Added file to repeatedly run string_params and string_parallel for different inputs

---
+++ week5/run_string.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run the file repeatedly with different numbers input
+# Using internal timing for benchmarking
+
+# Test for 20 to 1000 points (steps of 10)
+for i in {20..1000..10}; do
+	# Test for 10 to 1000 cycles
+	for j in {10..1000..10}; do
+		# Test for 10 to 500 samples
+		for k in {10..500..10}; do
+			./bin/string_params $i $j $k ./data/serial_out.csv
+			# For the parallel case check from 3 to 16 processors
+			for l in {3..16}; do
+				mpirun -np $l ./bin/string_parallel $i $j $k ./data/parallel_out.csv
+			done
+		done
+	done
+done
