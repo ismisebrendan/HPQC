a4cf62390489fa52b46d6c1ebf4f1358affcbc81
ismisebrendan <brendanwatters10@gmail.com>
2026-04-14 10:07:44 +0100

    Changed number of points, samples and timesteps in run_string

--- week5/run_string.sh
+++ week5/run_string.sh
@@ -3,12 +3,12 @@
 # Run the file repeatedly with different numbers input
 # Using internal timing for benchmarking
 
-# Test for 20 to 1000 points (steps of 10)
-for i in {20..1000..10}; do
-	# Test for 10 to 1000 cycles
-	for j in {10..1000..10}; do
-		# Test for 10 to 500 samples
-		for k in {10..500..10}; do
+# Test for 20 to 500 points (steps of 10)
+for i in {20..500..10}; do
+	# Test for 10 to 100 cycles
+	for j in {10..100..10}; do
+		# Test for 10 to 100 samples
+		for k in {10..100..10}; do
 			./bin/string_params $i $j $k ./data/serial_out.csv
 			# For the parallel case check from 3 to 16 processors
 			for l in {3..16}; do
